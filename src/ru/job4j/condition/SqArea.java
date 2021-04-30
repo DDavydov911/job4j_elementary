4d2b5cb6568aecb7ac6784df38f4f5a9e621550f
Denis Davydov <dvd9@yandex.ru>
2021-04-30 22:47:13 +0300

    4.5. Отладка программы в IDEA [#455756]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(int p, double k) {
         double h = p / (2.0 * (k + 1));
-        double l = h * k;
-        double s = l * h;
+        double w = h * k;
+        double s = w * h;
         return s;
     }
 
